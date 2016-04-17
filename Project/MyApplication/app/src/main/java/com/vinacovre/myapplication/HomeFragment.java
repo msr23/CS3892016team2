0a9028a9ce7f27e125254f31803e7c23337f6f97
vinacovre <viniassis@me.com>
2016-04-17 15:33:30 -0400

    R error solved

--- Project/MyApplication/app/src/main/java/com/vinacovre/myapplication/HomeFragment.java
+++ Project/MyApplication/app/src/main/java/com/vinacovre/myapplication/HomeFragment.java
@@ -1,12 +1,10 @@
 package com.vinacovre.myapplication;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 public class HomeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
@@ -64,8 +62,7 @@ public class HomeFragment extends Fragment {
 //    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
