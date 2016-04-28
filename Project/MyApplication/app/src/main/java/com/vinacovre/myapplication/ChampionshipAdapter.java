4cb0597b9983bf31e9cee54dc3b90da19430efba
Ryan Faranda <ryan.faranda3@gmail.com>
2016-04-28 17:43:41 -0400

    Added Firebase adapter
    In order to get the firebase to synch with the list view when the ui is
    created we must create a separate adapter that our ListView adapter will
    extend and implement.

--- Project/MyApplication/app/src/main/java/com/vinacovre/myapplication/ChampionshipAdapter.java
+++ Project/MyApplication/app/src/main/java/com/vinacovre/myapplication/ChampionshipAdapter.java
@@ -1,40 +1,68 @@
 package com.vinacovre.myapplication;
 
-import android.content.Context;
-import android.view.LayoutInflater;
+import android.app.Activity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
+import com.firebase.client.Query;
 /**
  * Created by Christian on 4/26/16.
  */
-public class ChampionshipAdapter extends ArrayAdapter<Championship> {
-   public ChampionshipAdapter(Context context, ArrayList<Championship> championships)
-   {
-       super(context,0,championships);
-   }
+public class ChampionshipAdapter extends FirebaseListAdapter<Championship> {
+  /*  public ChampionshipAdapter(Context context, int resource, ArrayList<Championship> championships)
+    {
+        super(context,resource,championships);
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         Championship champ = getItem(position);
+        //If thhis isnt working change convert view to View v, and then continue trying to mess around with thiss class
 
-        if(convertView == null)
-        {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view_layout, parent, false);
 
-        }
+        View v = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.list_view_layout,null);;
 
-        TextView tvName = (TextView) convertView.findViewById(R.id.championshipName);
-        TextView tvLocation = (TextView) convertView.findViewById(R.id.championshipLocation);
+        TextView tvName = (TextView) v.findViewById(R.id.championshipName);
+        TextView tvLocation = (TextView) v.findViewById(R.id.championshipLocation);
 
         tvName.setText(champ.getChampionshipName());
         tvLocation.setText(champ.getChampionshipLocation());
-        return  convertView;
+
+
+        if((convertView == null))
+        {
+            v = LayoutInflater.from(getContext()).inflate(R.layout.list_view_layout, parent, false);
+            //convertView = ((Activity)getContext()).getLayoutInflater().inflate(R.layout.list_view_layout,null);
+        }
+
+        return  v;
+    }*/
+
+
+
+   public ChampionshipAdapter(Query ref, Activity acitivity, int layout)
+   {
+       super(ref, Championship.class, layout, acitivity);
+
+   }
+
+    @Override
+    protected void populateView(View v, Championship championship) {
+        String championshipName = championship.getChampionshipName();
+        String championshipLocation = championship.getChampionshipLocation();
+
+        TextView championshipNameText = (TextView) v.findViewById(R.id.championshipName);
+        TextView championshipLocationText = (TextView) v.findViewById(R.id.championshipLocation);
+
+        championshipNameText.setText(championshipName + ":");
+        championshipLocationText.setText(championshipLocation + ":");
+
+
+
     }
+
+
+
 
 
 }
